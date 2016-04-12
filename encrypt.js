6bee5bf481e03daf5e882d4270fa02f34a387961
Matthew Roseman <mrr77@case.edu>
2016-04-12 16:10:30 -0400

    added openpgp javascript module and attempt to load it
    Signed-off-by: Matthew Roseman <mrr77@case.edu>

--- encrypt.js
+++ encrypt.js
@@ -1,6 +1,31 @@
 $(document).ready(function() {
-    $(".protocol_cbox").click(function() {
-        $(".protocol_cbox").prop("checked", false);
-        $(this).prop("checked", true);
+    openpgp.initWorker({path: './openpgp.worker.min.compiled.js'});
+    openpgp.config.aead_protect = true;
+
+    $('.protocol_cbox').click(function() {
+        $('.protocol_cbox').prop('checked', false);
+        $(this).prop('checked', true);
+    });
+
+    $('.encrypt-button').click(function() {
+        if ($('.cbox_rsa').is(':checked')) {
+            printRsaEncryption($('.plaintext-message').val());
+        }
+        else if ($('.cbox_sha').is(':checked')) {
+            printShaEncryption($('.plaintext-message').val());
+        }
     });
 });
+
+function printRsaEncryption(plaintext) {
+    var options = {
+        data: plaintext
+    };
+    openpgp.encrypt(options).then(function(ciphertext) {
+        encrypted = ciphertext.data;
+    });
+    $('body').append(encrypted);
+}
+
+function printShaEncryption(plaintext) {
+}
